6291742f94e7087b8d1bbac6f7ba0610aa04504a
AndNovv <andrew12011976@mail.ru>
2024-03-28 11:39:30 +0300

    ui: flashcard animation

--- src/components/Learning/FlashCard.tsx
+++ src/components/Learning/FlashCard.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FlashCardType } from '@/types/types'
 import { motion } from 'framer-motion'
 
@@ -15,12 +15,27 @@ const FlashCard = ({ flashcardInfo }: { flashcardInfo: FlashCardType }) => {
         }
     }
 
+    useEffect(() => {
+        setIsFlipped(false)
+    }, [flashcardInfo])
 
     return (
-        <div onClick={handleCardClick} className='flip-card cursor-pointer h-40 w-1/2'>
+        <motion.div
+            key={`flashcard-${flashcardInfo._id}`}
+            initial={{ translateY: -10, opacity: 0 }}
+            animate={{ translateY: 0, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            onClick={handleCardClick}
+            className='flip-card cursor-pointer h-40 w-1/2'
+        >
             <motion.div
-                initial={false}
-                animate={{ rotateY: isFlipped ? 180 : 360 }}
+                key={`flashcardInner-${flashcardInfo._id}`}
+                initial={'front'}
+                animate={isFlipped ? 'back' : 'front'}
+                variants={{
+                    front: { rotateY: 360 },
+                    back: { rotateY: 180 },
+                }}
                 transition={{ duration: 0.3, animationDirection: 'normal' }}
                 onAnimationComplete={() => setIsAnimating(false)}
                 className='flip-card-inner w-full h-full text-2xl'
@@ -28,7 +43,7 @@ const FlashCard = ({ flashcardInfo }: { flashcardInfo: FlashCardType }) => {
                 <div className='flip-card-front flex w-full h-full bg-card rounded-lg border shadow-md shadow-card justify-center items-center p-4'><p>{flashcardInfo.english}</p></div>
                 <div className='flip-card-back flex w-full h-full bg-card rounded-lg border shadow-md shadow-card justify-center items-center p-4'><p>{flashcardInfo.russian}</p></div>
             </motion.div>
-        </div>
+        </motion.div>
 
     )
 }
