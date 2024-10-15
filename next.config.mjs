695f1c6cc1141e09f9fd4a0e4da83ff7b6e23ed3
AndNovv <andrew12011976@mail.ru>
2024-10-15 20:54:25 +0300

    next config fixes

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,18 @@ const nextConfig = {
         styledComponents: true
     },
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '*',
-                port: '',
-            },
-        ],
+        path: '/_next/image',
+    // loader can be 'default', 'imgix', 'cloudinary', 'akamai', or 'custom'
+        loader: 'default',
+    // file with `export default function loader({src, width, quality})`
+        loaderFile: '',
+    // disable static imports for image files
+        disableStaticImages: false,
+    // minimumCacheTTL is in seconds, must be integer 0 or more
+        minimumCacheTTL: 60,
+    // ordered list of acceptable optimized image formats (mime types)
+        formats: ['image/webp'],
+        remotePatterns: [],
     },
     webpack: (config, { isServer }) => {
         config.module.rules.push({
