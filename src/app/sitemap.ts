6edb4d33c4efe0f927e0b9d5e47def063b3e162c
AndNovv <andrew12011976@mail.ru>
2024-10-15 17:51:13 +0300

    edited sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,34 +1,68 @@
-import dbConnect from '@/lib/mongo/dbConnect'
-import BlogPost from '@/models/BlogPost'
-import { MetadataRoute } from 'next'
+// import dbConnect from '@/lib/mongo/dbConnect'
+// import BlogPost from '@/models/BlogPost'
+// import { MetadataRoute } from 'next'
 
-const baseUrl = 'https://plexicon.ru'
+// const baseUrl = 'https://plexicon.ru'
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
-    await dbConnect()
-    const blogPosts = await BlogPost.find({})
+//     await dbConnect()
+//     const blogPosts = await BlogPost.find({})
 
-    const baseSitemap = [
-        {
-            url: 'https://plexicon.ru',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://plexicon.ru/collections',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://plexicon.ru/blog',
-            lastModified: new Date(),
-        },
-    ]
+//     const baseSitemap = [
+//         {
+//             url: 'https://plexicon.ru',
+//             lastModified: new Date(),
+//         },
+//         {
+//             url: 'https://plexicon.ru/collections',
+//             lastModified: new Date(),
+//         },
+//         {
+//             url: 'https://plexicon.ru/blog',
+//             lastModified: new Date(),
+//         },
+//     ]
 
-    const blogPostsArray = blogPosts.map((blogPost) => ({
-        url: `${baseUrl}/blog/${blogPost._id}`,
-        lastModified: new Date(),
-    }))
+//     const blogPostsArray = blogPosts.map((blogPost) => ({
+//         url: `${baseUrl}/blog/${blogPost._id}`,
+//         lastModified: new Date(),
+//     }))
 
 
-    return [...baseSitemap, ...blogPostsArray]
+//     return [...baseSitemap, ...blogPostsArray]
+// }
+
+
+import dbConnect from '@/lib/mongo/dbConnect';
+import BlogPost from '@/models/BlogPost';
+
+const baseUrl = 'https://plexicon.ru';
+
+// Generate all dynamic paths at build time
+export const generateStaticParams = async () => {
+    await dbConnect();
+    const blogPosts = await BlogPost.find({}, '_id'); // Only fetch `_id`
+
+    return blogPosts.map((post) => ({
+        id: post._id.toString(), // Convert MongoDB ObjectId to string
+    }));
+};
+
+// Sitemap generation function for metadata route
+export default async function sitemap() {
+    const posts = await generateStaticParams(); // Get dynamic routes
+
+    const staticRoutes = [
+        { url: `${baseUrl}`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/collections`, lastModified: new Date().toISOString() },
+        { url: `${baseUrl}/blog`, lastModified: new Date().toISOString() },
+    ];
+
+    const dynamicRoutes = posts.map((post) => ({
+        url: `${baseUrl}/blog/${post.id}`,
+        lastModified: new Date().toISOString(),
+    }));
+
+    return [...staticRoutes, ...dynamicRoutes];
 }
--- src/lib/mongo/dbConnect.ts
+++ src/lib/mongo/dbConnect.ts
@@ -2,21 +2,13 @@ import mongoose from "mongoose";
 
 const connecition: { isConnected?: number } = {}
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-    throw new Error('Missing MONGODB_URI in environment variables');
-}
-
-console.log(MONGODB_URI)
-
 async function dbConnect() {
 
     if (connecition.isConnected) {
         return
     }
 
-    const db = await mongoose.connect(MONGODB_URI!)
+    const db = await mongoose.connect(process.env.MONGODB_URI!)
 
     connecition.isConnected = db.connections[0].readyState
 }
